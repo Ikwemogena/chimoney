b7789a3e06cc5f113d47b8a529509acc1daefa15
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-05 02:26:14 +0100

    feat: added login functionality

--- server/src/controller/authentication.ts
+++ server/src/controller/authentication.ts
@@ -28,3 +28,27 @@ export const registerUser = async (req: express.Request, res: express.Response) 
         return res.status(400).send({ message: 'Error creating user' });
     }
 }
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).send({ message: 'Missing fields' });
+        }
+
+        const user = await getUserByEmail(email)
+
+        if (!user) {
+            return res.status(400).send({ message: 'User not found' })
+        }
+
+        if (user.password !== password.toString()) {
+            return res.status(403).send({ message: 'Invalid username/password' })
+        }
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        return res.status(400).send({ message: 'Error logging in' });
+    }
+}
