ce9ee8fd78de5a83175c8edd89e89871c337daf2
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-05 16:59:29 +0100

    feat: Refactor user registration process and add login functionality with supbase

--- server/src/repository/users.ts
+++ server/src/repository/users.ts
@@ -1,15 +1,44 @@
-import { client } from "../index";
+import { supabase } from "../services/supabase";
 
-export const createUser = async (name: string, email: string, password: string) => {
-    const { rows } = await client.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, password]);
-    return rows;
+export const signUpUser = async (email: string, password: string) => {
+    try {
+        const { data: user, error } = await supabase.auth.signUp({
+            email,
+            password
+        })
+
+        if (user) {
+            return user.user
+        } else {
+            console.log(error, 'cannot add user')
+            return error
+        }
+    } catch (error) {
+        return null
+    }
 }
 
-export const getUserByEmail = async (email: string) => {
+export const addUserToDb = async (user: object) => {
     try {
-        const { rows } = await client.query('SELECT * FROM users WHERE email = $1', [email]);
-        return rows[0];
+        await supabase
+            .from('users')
+            .insert([user])
+            .select()
     } catch (error) {
-        return null;
+        return error
+    }
+
+}
+
+export const getExistingUser = async (email: string) => {
+    try {
+        const { data: existingUser, error } = await supabase
+            .from('users')
+            .select("*")
+            .eq('email', email)
+            .single()
+        return existingUser
+    } catch (error) {
+        return null
     }
 }
