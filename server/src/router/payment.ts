4c870019a6a710aa0735fe128373c94edf326e20
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-06 23:19:19 +0100

    Add authentication middleware to payment and wallet routes

--- server/src/router/payment.ts
+++ server/src/router/payment.ts
@@ -1,7 +1,8 @@
 
 import express from 'express';
 import { sendPaymentRequest } from '../controller/transactions';
+import { isAuthenticated } from '../middleware';
 
 export default (router: express.Router) => {
-    router.post('/request-payment', sendPaymentRequest)
+    router.post('/request-payment', isAuthenticated, sendPaymentRequest)
 }
--- server/src/router/wallet.ts
+++ server/src/router/wallet.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getUserWallet, makeMoneyTransfer } from '../controller/wallet';
 import { initiatePayout } from '../controller/transactions';
+import { isAuthenticated, isOwner } from '../middleware';
 
 export default (router: express.Router) => {
-    router.get('/wallet/:id', getUserWallet)
-    router.post('/wallet/transfer', makeMoneyTransfer)
-    router.post('/wallet/payout', initiatePayout)
+    router.get('/wallet/:id', isAuthenticated, isOwner, getUserWallet)
+    router.post('/wallet/transfer', isAuthenticated, makeMoneyTransfer)
+    router.post('/wallet/payout', isAuthenticated, initiatePayout)
 }
