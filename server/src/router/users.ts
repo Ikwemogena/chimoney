705596712e6d3d49a256fa0dd4dc9d46bd91546d
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-06 23:16:39 +0100

    feat: added authentication to user transaction endpoint

--- server/src/router/users.ts
+++ server/src/router/users.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getAllUserTransactions } from '../controller/transactions';
+import { isAuthenticated, isOwner } from '../middleware';
 
 export default (router: express.Router) => {
-    router.get('/user/transactions/:id', getAllUserTransactions)
+    router.get('/user/transactions/:id', isAuthenticated, isOwner, getAllUserTransactions)
 }
