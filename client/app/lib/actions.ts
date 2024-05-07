47336bced674a710c82aaf1fbacc75d1e6e6be1f
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-07 16:01:06 +0100

    feat: Add login functionality and user transaction retrieval

---
+++ client/app/lib/actions.ts
@@ -0,0 +1,53 @@
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation'
+
+export async function login(prevState: any, formData: FormData) {
+
+    const credentials = {
+        email: formData.get('email'),
+        password: formData.get('password')
+    }
+
+    const endpoint = 'http://localhost:8080/auth/login'
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        cookies().set('accessToken', data.access_token, {
+            maxAge: 60 * 60 * 24,
+        });
+        cookies().set('user', JSON.stringify(data.user), {
+            maxAge: 60 * 60 * 24,
+        })
+        redirect('/');
+    } else {
+        return { error: 'Invalid username/password' };
+    }
+}
+
+export const fetchUserTransactions = async (id: string) => {
+    const token = cookies().get('accessToken')?.value
+    try {
+        const endpoint = `http://localhost:8080/user/transactions/${id}`
+        const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log(response.status)
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error
+    }
+}
