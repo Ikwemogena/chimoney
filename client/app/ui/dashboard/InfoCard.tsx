66a31c0a014f662cfc08b300ae4043ce58aecd12
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-09 15:16:40 +0100

    chore: Update InfoCard component to display label and value

--- client/app/ui/dashboard/InfoCard.tsx
+++ client/app/ui/dashboard/InfoCard.tsx
@@ -6,8 +6,9 @@ interface InfoItemProps {
 export default function InfoCard({ value, label }: InfoItemProps) {
     return (
         <div className="dashboard__info-item">
-            <h3 className="dashboard__info-value">{value}</h3>
+
             <p className="dashboard__info-label">{label}</p>
+            <p className="dashboard__info-value">{value}</p>
         </div>
     )
 };
