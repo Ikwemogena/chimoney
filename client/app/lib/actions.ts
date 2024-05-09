ce85e9119457e402ea79bd26f6fedbee68daeeec
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-09 18:55:46 +0100

    chore: Update fetchUserTransactions to use baseUrl for API endpoints

--- client/app/lib/actions.ts
+++ client/app/lib/actions.ts
@@ -2,13 +2,40 @@
 import { cookies } from "next/headers";
 import { redirect } from 'next/navigation'
 
+console.log(process.env.BASE_URL)
+
+const baseUrl = process.env.BASE_URL
+
+
+const useFetch = async (url: string, method: string, body: any) => {
+    const token = cookies().get('accessToken')?.value
+    try {
+        const response = await fetch(url, {
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log(response.status)
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error
+    }
+}
+
 export async function login(prevState: any, formData: FormData) {
     const credentials = {
         email: formData.get('email'),
         password: formData.get('password')
     }
 
-    const endpoint = 'http://localhost:8080/auth/login'
+    // const endpoint = 'http://localhost:8080/auth/login'
+    const endpoint = `${baseUrl}/auth/login`
     const response = await fetch(endpoint, {
         method: 'POST',
         body: JSON.stringify(credentials),
@@ -32,9 +59,11 @@ export async function login(prevState: any, formData: FormData) {
 }
 
 export const fetchUserTransactions = async (id: string) => {
+    const baseUrl = process.env.BASE_URL
     const token = cookies().get('accessToken')?.value
     try {
-        const endpoint = `http://localhost:8080/user/transactions/${id}`
+        // const endpoint = `http://localhost:8080/user/transactions/${id}`
+        const endpoint = `${baseUrl}/user/transactions/${id}`
         const response = await fetch(endpoint, {
             method: 'GET',
             headers: {
@@ -43,7 +72,7 @@ export const fetchUserTransactions = async (id: string) => {
             },
         });
         if (!response.ok) {
-            cookies().delete('accessToken')
+            // cookies().delete('accessToken')
             console.log(response.status)
         }
         const data = await response.json();
