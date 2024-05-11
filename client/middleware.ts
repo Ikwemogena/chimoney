e9c046b88c9f1a1f6895595ae49c31723c9a4f17
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-12 00:11:46 +0100

    fix: Update middleware to handle authentication and redirection

--- client/middleware.ts
+++ client/middleware.ts
@@ -14,21 +14,25 @@ export const config = {
     ],
 }
 
-export function middleware(request: NextRequest) {
 
+export function middleware(request: NextRequest) {
     const isLoggedIn = request.cookies.get('accessToken')
 
-    if (!isLoggedIn && !request.nextUrl.pathname.includes('/auth')) {
-        return NextResponse.redirect(new URL("/auth/sign-in", request.url));
-    }
+    if (!isLoggedIn) {
+        if (request.nextUrl.pathname === '/auth/sign-up') {
+            return NextResponse.next();
+        } else if (!request.nextUrl.pathname.includes('/auth')) {
+            return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+        }
 
-    if (isLoggedIn && request.nextUrl.pathname.includes('/auth/sign-in') || request.nextUrl.pathname.includes('/auth/sign-up')) {
-        return NextResponse.redirect(new URL("/", request.url))
-    }
+        if (isLoggedIn && (request.nextUrl.pathname.includes('/auth/sign-in') || request.nextUrl.pathname.includes('/auth/sign-up'))) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
 
-    if (isLoggedIn && request.nextUrl.pathname === '/auth/sign-out') {
-        return NextResponse.redirect(new URL("/auth/sign-in", request.url));
-    }
+        if (isLoggedIn && request.nextUrl.pathname === '/auth/sign-out') {
+            return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+        }
 
-    return NextResponse.next();
+        return NextResponse.next();
+    }
 }
