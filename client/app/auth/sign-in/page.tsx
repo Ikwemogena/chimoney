113cacb1b48ce903bf7615e16c1fff06b82261f7
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-11 23:41:43 +0100

    chore: Update sign-in page submit button text to "Sign In"

--- client/app/auth/sign-in/page.tsx
+++ client/app/auth/sign-in/page.tsx
@@ -20,7 +20,6 @@ function page() {
         <Box height={'100vh'} width={'100%'} display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"}>
             <Box display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"space-between"} gap={6} padding={5}>
                 <Box display={"flex"} flexDirection={"column"} gap={1} width={"100%"}>
-                    {/* <Image src="https://chimoney.io/assets/icons/chimoney-purple-logo.svg" alt="Chimoney Logo" width={100} height={100} /> */}
                     <Typography variant="h4" fontWeight={700}>Welcome back,</Typography>
                     <Typography variant="subtitle2" color={"GrayText"}>Sign in to your account to continue</Typography>
                 </Box>
@@ -30,12 +29,8 @@ function page() {
                             <TextField id="outlined-basic" name="email" label="Email" type="email" variant="outlined" fullWidth required />
                             <TextField id="outlined-basic" name="password" label="Password" type="password" variant="outlined" fullWidth required />
                         </Box>
-                        <Box display={"flex"} justifyContent={"space-between"} paddingTop={"1rem"}>
-                            <Typography variant="subtitle1">Remember me</Typography>
-                            <Typography variant="subtitle1">Forgot password?</Typography>
-                        </Box>
                         <Box display={"flex"} marginTop={5}>
-                            <SubmitButton text="Login" />
+                            <SubmitButton text="Sign In" />
                         </Box>
                     </form>
                 </Box>
