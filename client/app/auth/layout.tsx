681ac4808af119b64fa9f6f68ff00d791e324540
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-05-11 23:39:20 +0100

    chore: Update auth layout and styling

--- client/app/auth/layout.tsx
+++ client/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import Image from "next/image";
 
 export default function AuthLayout({
     children,
@@ -11,8 +12,9 @@ export default function AuthLayout({
             display="flex"
             alignItems="center"
         >
-            <Box bgcolor="#670b78" height={'100vh'} width={'100%'}>
-                Image here
+            <Box className="auth-image" bgcolor="#670b78" height={'100vh'} width={'100%'} overflow={"hidden"}>
+                {/* <Image src={'https://images.unsplash.com/photo-1593182440959-9d5165b29b59?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} width={100} height={100} layout="responsive" quality={100}
+                    alt="Auth Image" /> */}
             </Box>
             {children}
 
